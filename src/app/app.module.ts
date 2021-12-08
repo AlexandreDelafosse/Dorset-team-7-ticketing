5a68dc2122763e3ba7f2f1baec045bcac8cfb5e4
Simon Roche <rochesimon92@gmail.com>
2021-12-08 11:56:53 +0000

    connect database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,28 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat/';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore/';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIZ79X39JxBBiDxUlc12zv08Ujbpi_mfc",
+  authDomain: "ticketing-6ad1e.firebaseapp.com",
+  projectId: "ticketing-6ad1e",
+  storageBucket: "ticketing-6ad1e.appspot.com",
+  messagingSenderId: "20890478415",
+  appId: "1:20890478415:web:a7c0c7f5208f438effc73a"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
